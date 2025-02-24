48ab45c95344a0465aceafc9a4c36cf59fd173cd
rulercosta <rulercosta.mail@gmail.com>
2025-02-25 02:19:43 +0530

    enhance functionality: simplify TouchManager by removing detailView and onSwipeBack parameters

--- static/components/TouchManager.js
+++ static/components/TouchManager.js
@@ -1,82 +1,20 @@
 export class TouchManager {
     constructor(detailView, notesList, onSwipeBack, onDeleteNote) {
-        this.detailView = detailView;
         this.notesList = notesList;
-        this.onSwipeBack = onSwipeBack;
         this.onDeleteNote = onDeleteNote;
         
-        this.touchStartX = 0;
-        this.touchStartY = 0;
-        this.touchMoveX = 0;
-        this.isSwiping = false;
         this.swipeStartX = null;
         this.swipeStartY = null;
         this.currentSwipeItem = null;
         this.swipeThreshold = -80;
-        this.isAndroid = /Android/.test(navigator.userAgent);
 
         this.setupEventListeners();
     }
 
     setupEventListeners() {
-        this.detailView.addEventListener('touchstart', (e) => this.handleTouchStart(e));
-        this.detailView.addEventListener('touchmove', (e) => this.handleTouchMove(e));
-        this.detailView.addEventListener('touchend', (e) => this.handleTouchEnd(e));
-        
         this.notesList.addEventListener('touchstart', (e) => this.handleNoteSwipeStart(e));
         this.notesList.addEventListener('touchmove', (e) => this.handleNoteSwipeMove(e));
         this.notesList.addEventListener('touchend', (e) => this.handleNoteSwipeEnd(e));
-    }
-
-    handleTouchStart(e) {
-        if (e.target.closest('.editor-container')) return;
-        this.touchStartX = e.touches[0].clientX;
-        this.touchStartY = e.touches[0].clientY;
-        this.touchMoveX = 0;
-        this.isSwiping = false;
-    }
-
-    handleTouchMove(e) {
-        if (!this.touchStartX || e.target.closest('.editor-container')) return;
-        
-        const touchX = e.touches[0].clientX;
-        const touchY = e.touches[0].clientY;
-        const deltaX = touchX - this.touchStartX;
-        const deltaY = Math.abs(touchY - this.touchStartY);
-        
-        if (!this.isSwiping && Math.abs(deltaX) > deltaY && Math.abs(deltaX) > 10) {
-            this.isSwiping = true;
-        }
-        
-        if (this.isSwiping) {
-            e.preventDefault();
-            this.touchMoveX = deltaX;
-            
-            if (this.isAndroid) {
-                const opacity = Math.max(0, 1 - (deltaX / 200));
-                this.detailView.style.transform = `translateX(${Math.max(deltaX, 0)}px)`;
-                this.detailView.style.opacity = opacity;
-            } else {
-                this.detailView.style.transform = `translateX(${Math.max(deltaX, 0)}px)`;
-            }
-        }
-    }
-
-    handleTouchEnd(e) {
-        if (!this.isSwiping) return;
-        
-        if (this.touchMoveX > 100) {
-            this.onSwipeBack();
-        } else {
-            this.detailView.style.transform = '';
-            if (this.isAndroid) {
-                this.detailView.style.opacity = '';
-            }
-        }
-        
-        this.touchStartX = 0;
-        this.touchMoveX = 0;
-        this.isSwiping = false;
     }
 
     handleNoteSwipeStart(e) {
